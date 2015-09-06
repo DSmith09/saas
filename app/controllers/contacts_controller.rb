6aef88581338227d3a35e0fd39bb2892ff0d7c93
David Smith <dmsmithswe@gmail.com>
2015-09-06 23:09:15 +0000

    Added functionality for contacts/new page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,9 @@
-class ContactsController < ApplicationsController
+class ContactsController < ApplicationController
     
+    #This declares the variables used in the View for the contacts/new page
+    #The object Contact is defined in the Contact Model
     def new
-        
+       @contact = Contact.new 
     end
     
     def create 
