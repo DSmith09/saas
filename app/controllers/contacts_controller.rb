c970fff04d4005974f839f8954f263564921d308
David Smith <dmsmithswe@gmail.com>
2015-09-09 05:41:34 +0000

    Added Validations and Create Action

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,6 +7,22 @@ class ContactsController < ApplicationController
     end
     
     def create 
-    
+        @contact = Contact.new(contact_params)
+        
+        #If Data is submitted/saved on contacts page, re-open the contacts/new page
+        if @contact.save
+            flash[:success] = "Message sent."
+            redirect_to new_contact_path 
+            #This occurs only if the page fails
+        else 
+            flash[:danger] = "Error occurred during post"
+            redirect_to new_contact_path
+        end
     end
+    
+    #White Listing for Query String Parameters
+    private 
+        def contact_params
+            params.require(:contact).permit(:name, :email, :comments)
+        end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,6 @@
 class Contact < ActiveRecord::Base
-    
+    #Validates that input is present when submitted to database 
+    validates :name, presence: true
+    validates :email, presence: true
 end
     
