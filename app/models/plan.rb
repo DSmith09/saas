c65f523da369839174384bb3ed305f18fc7467dc
David Smith <dmsmithswe@gmail.com>
2015-10-23 20:04:29 +0000

    Added Association for Plans and Users

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,3 +1,4 @@
 class Plan < ActiveRecord::Base
-    
+    #Associates the different types of Plans with a User
+    has_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
+  #Associates a User with a Plan
+  belongs_to :plan
 end
