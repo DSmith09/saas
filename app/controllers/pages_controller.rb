70f72a8ad7e0db4af07c4d76a8116ebbcdfcef59
David Smith <dmsmithswe@gmail.com>
2015-10-25 14:32:19 +0000

    Added plan variables to button press on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
     def home
-        
+    #This allows me to identify which Plan to use in the event of a button press from the home page
+    #Find is based on the id in the Plan DB Table
+       @basic_plan = Plan.find(1)
+       @pro_plan = Plan.find(2)
     end
     
     def about
